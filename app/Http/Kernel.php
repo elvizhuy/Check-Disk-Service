8f14332ee39176beda824d3496dc1ec4625df29c
elvizhuy <elvizhuy@gmail.com>
2023-07-17 09:39:44 +0700

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,9 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 //            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            'throttle:60000:1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
         ],
     ];
 
