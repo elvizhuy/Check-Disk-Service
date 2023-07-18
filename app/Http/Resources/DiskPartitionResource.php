f290cf5de1c50edc38be8799406b4d070f2d2867
HuyNg <elvizhuy@gmail.com>
2023-07-18 20:32:56 +0700

    .

--- app/Http/Resources/DiskPartitionResource.php
+++ app/Http/Resources/DiskPartitionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,6 +16,7 @@ class DiskPartitionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $dt = Carbon::now('Asia/Ho_Chi_Minh');
         return [
             'file_system' => $this->file_system,
             'size'=> $this->size,
@@ -22,7 +24,7 @@ class DiskPartitionResource extends JsonResource
             'available' => $this->available,
             'use_percentage' => $this->use_percentage,
             'belongtoVirtualMachine' =>$this->belongtoVirtualMachine,
-            'created_at' => (new \DateTime($this->created_at))->format('d-m-Y H:m:s'),
+            'created_at' => (new \DateTime($this->created_at))->format('d-m-Y'),
             'updated_at' => (new \DateTime($this->updated_at))->format('d-m-Y H:m:s'),
         ];
     }
