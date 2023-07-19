005a16f44f6de5c80c51b96f3c66e92b1ba56c31
elvizhuy <elvizhuy@gmail.com>
2023-07-19 09:27:56 +0700

    ..

--- app/Http/Resources/DiskPartitionResource.php
+++ app/Http/Resources/DiskPartitionResource.php
@@ -16,8 +16,8 @@ class DiskPartitionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // $dt = Carbon::now()->tz('Asia/Ho_Chi_Minh');
-        $datetime = new Carbon('YYYY-MM-DD HH:II:SS', 'Asia/Bangkok');
+        $dt = Carbon::now('Asia/Ho_Chi_Minh');
+        // $datetime = new Carbon('Y-M-D H:I:S', 'Asia/Bangkok');
             // ->settings(['timezone' => 'Asia/Bangkok']);
         return [
             'file_system' => $this->file_system,
@@ -26,9 +26,11 @@ class DiskPartitionResource extends JsonResource
             'available' => $this->available,
             'use_percentage' => $this->use_percentage,
             'belongtoVirtualMachine' => $this->belongtoVirtualMachine,
-            'created_at' => $datetime->toDayDateTimeString(),
+            'created_at' => $dt->toDateTimeString(),
+            'updated_at' => $dt->toDateTimeString()
+            // 'created_at' => $dt->toDayDateTimeString(),
             // 'created_at' => (new \DateTime($this->created_at))->format('d-m-Y H:m:s'),
-            'updated_at' => (new \DateTime($this->updated_at))->format('d-m-Y H:m:s'),
+            // 'updated_at' => (new \DateTime($this->updated_at))->format('d-m-Y H:m:s'),
         ];
     }
 }
